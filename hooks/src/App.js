40b91014f0b1c1b11556858bf4e66d0342d41772
fahim2077 <fahimalsafi@gmail.com>
2020-12-25 01:17:35 -0500

    init

---
+++ hooks/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Accordion from './components/Accordion.js';
+
+const items = [
+  {
+    title : "What is Angular?",
+    content : "Angular is a TypeScript-based open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations."
+  },{
+    title : "What is React?",
+    content : "React is an open-source, front end, JavaScript library for building user interfaces or UI components"
+  },{
+    title : "What is Vue?",
+    content : "Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications."
+  }
+]
+
+
+const App = () => {
+  return <div>
+  <Accordion items={items} />
+  </div>
+};
+
+export default App;

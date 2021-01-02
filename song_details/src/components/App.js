4540691cf2d3e4db8526969ae49c390f31b859a0
fahim2077 <fahimalsafi@gmail.com>
2021-01-02 10:03:51 -0500

    formatting

--- song_details/src/components/App.js
+++ song_details/src/components/App.js
@@ -13,7 +13,14 @@ const App = () => {
           <SongDetails />
         </div>
       </div>
-    </div>
+	  <div>
+	    <small>You are running this application in <b>development</b> mode.
+		{process.env.REACT_APP_NOT_SECRET_CODE}</small>
+	    <form>
+	      <input type="hidden" value="abcdef" />
+	    </form>
+	  </div>
+	 </div>
   );
 };
 

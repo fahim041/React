1f699e34ff5ca9ff6b9c390323c9507db50a46f8
Sami2077 <fahimalsafi@gmail.com>
2021-01-26 23:55:49 -0500

    facebook clone ui

---
+++ facebook/src/Feed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Feed.css";
+import MessageSender from './MessageSender';
+import StoryReel from './StoryReel';
+
+const Feed = () => {
+    return(
+        <div className="feed">
+            <StoryReel />
+            <MessageSender />
+        </div>
+    )
+};
+
+export default Feed;
---
+++ facebook/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKKFB0PCk0bLfu7REE7ElleTWDhu_Eawo",
+    authDomain: "fb-clone-8cc68.firebaseapp.com",
+    projectId: "fb-clone-8cc68",
+    storageBucket: "fb-clone-8cc68.appspot.com",
+    messagingSenderId: "332855369020",
+    appId: "1:332855369020:web:156fb1f90506b26d9fd239",
+    measurementId: "G-QJWTRNZ437"
+  };

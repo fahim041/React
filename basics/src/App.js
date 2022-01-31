8ccfa7ddd9b32f8cbb169e930bd37f7a1532d706
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2022-01-31 18:20:36 -0500

    sync

---
+++ basics/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counters from "./components/counters";
+
+function App() {
+  return (
+    <main className="container">
+      <Counters />
+    </main>
+  );
+}
+
+export default App;
---
+++ basics/src/components/counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm">
+          Increase
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}>
+          Delete
+        </button>
+      </>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
